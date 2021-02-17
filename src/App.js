52464955db0a16f0eb9a73b71cf05f4e1667fffd
swimming_kim <tge1375@naver.com>
2021-02-18 05:17:30 +0900

    Upto 3.1
    Signed-off-by: swimming_kim <tge1375@naver.com>

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-// props.name == { name }
-function Movie({ name, image }) {
-  return <h1>{name}</h1>;
-}
-
-Movie.propTypes = {
-  name: PropTypes.string.isRequired,
-};
-
-const menu = [
-  {
-    key: 1,
-    name: "Sex on the City",
-    image: "",
-  },
-  {
-    key: 2,
-    name: "Home Alone",
-    image: "",
-  },
-  {
-    key: 3,
-    name: "Avengers",
-    image: "",
-  },
-  {
-    key: 4,
-    name: "My Life",
-    image: "",
-  },
-  {
-    key: 5,
-    name: 12,
-    image: "",
-  },
-];
-
-const renderMovie = (ticket) => {
-  return (
-    <Movie key={ticket.key} name={ticket.name} image={ticket.image}></Movie>
-  );
-};
-
-function App() {
-  return (
-    <div>
-      Fuck you !!
-      {menu.map(renderMovie)}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
