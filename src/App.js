00645d74843702e0ed2e1f0428581932543ed8d7
swimming_kim <tge1375@naver.com>
2021-02-18 04:56:01 +0900

    Upto Lesson2
    Signed-off-by: swimming_kim <tge1375@naver.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
+// props.name == { name }
+function Movie({ name, image }) {
+  return <h1>{name}</h1>;
+}
+
+Movie.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+const menu = [
+  {
+    key: 1,
+    name: "Sex on the City",
+    image: "",
+  },
+  {
+    key: 2,
+    name: "Home Alone",
+    image: "",
+  },
+  {
+    key: 3,
+    name: "Avengers",
+    image: "",
+  },
+  {
+    key: 4,
+    name: "My Life",
+    image: "",
+  },
+  {
+    key: 5,
+    name: 12,
+    image: "",
+  },
+];
+
+const renderMovie = (ticket) => {
+  return (
+    <Movie key={ticket.key} name={ticket.name} image={ticket.image}></Movie>
+  );
+};
 
 function App() {
-  return (<div className="App" > Fuck you !!</div>);
+  return (
+    <div>
+      Fuck you !!
+      {menu.map(renderMovie)}
+    </div>
+  );
 }
 
 export default App;
